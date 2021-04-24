2e3923002c4b0373052cfce63ac40c4df1ec9f82
lilneuromancer <neuromancer.kitten@gmail.com>
2021-04-23 17:34:59 -0700

    updated exporter

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import Head from 'next/head'
 import { Layout } from '../components'
-import ExploreView from './ExploreView'
+import Mint from '../components/Mint'
 
 export default function Home() {
   return (
     <Layout>
       <div className={'w-11/12 max-w-6xl mx-auto'}>
-        <ExploreView />
+        <Mint />
       </div>
     </Layout>
   )
