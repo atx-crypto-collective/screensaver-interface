587b855688e00853ff6ad84abe80b7f8e6cd22de
lilneuromancer <neuromancer.kitten@gmail.com>
2021-04-29 22:57:58 -0700

    removed storybook

--- stories/components/RoundGrayBox.stories.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Meta } from '@storybook/react'
-
-export const Default = () => <div className={'round-gray-box'}>Contents</div>
-
-export default {
-  title: 'Components/Round Gray Box',
-} as Meta
