15cfb397f86b1c400109458fba17c5725f378259
lilneuromancer <neuromancer.kitten@gmail.com>
2021-05-11 16:48:41 -0700

    twitter card fix

--- pages/_document.js
+++ pages/_document.js
@@ -18,6 +18,17 @@ class MyDocument extends Document {
             noModule
             src="https://unpkg.com/@google/model-viewer/dist/model-viewer-legacy.js"
           />
+
+          {/* Twitter */}
+          <meta name="twitter:card" content="summary" key="twcard" />
+          <meta name="twitter:creator" content={'@screensaverdao'} key="twhandle" />
+
+          {/* Open Graph */}
+          <meta property="og:url" content={`https://www.screensaver.world/gallery`} key="ogurl" />
+          <meta property="og:image" content={''} key="ogimage" />
+          <meta property="og:site_name" content={'Screensaver Dao'} key="ogsitename" />
+          <meta property="og:title" content={'Screensaver'} key="ogtitle" />
+          <meta property="og:description" content={'Screensaver Dao Auction'} key="ogdesc" />
         </Head>
         <body>
           <Main />
