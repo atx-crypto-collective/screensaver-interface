d2202dfae53cce1843ee869833cfbe7f005873fc
lilneuromancer <neuromancer.kitten@gmail.com>
2021-06-05 21:32:44 -0700

    set sale adding back

--- components/BidRow.tsx
+++ components/BidRow.tsx
@@ -192,6 +192,9 @@ const BidRow: React.VFC<IProps> = ({ tokenId }) => {
       >No Bids Yet</div>
     
     }
+          {!ownerOf && (
+        <SetSalePrice sale={false} tokenId={tokenId} onUpdate={currentBids} />
+      )}
     </>
   )
 }
