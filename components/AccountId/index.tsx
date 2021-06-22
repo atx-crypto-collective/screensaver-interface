10ddd2ab65ae6885a6e44b0f43c807a54dca5fb6
Brandon Valosek <bvalosek@gmail.com>
2021-06-22 17:48:12 -0500

    update AccountId component to optionally point to created page

--- components/AccountId/index.tsx
+++ components/AccountId/index.tsx
@@ -2,7 +2,12 @@ import React, { useEffect } from 'react'
 import useAxios from 'axios-hooks'
 import { shortenAddress } from '../../utils'
 
-export default function AccountId({address}) {
+interface Props {
+    address: string
+    linkToCreated?: boolean
+}
+
+export default function AccountId({address, linkToCreated}: Props) {
     const [{ data, loading, error }, refetch] = useAxios(
         `https://us-central1-proofoftwitter.cloudfunctions.net/api/user/?address=${address}`
       )
@@ -10,14 +15,16 @@ export default function AccountId({address}) {
       useEffect(() => {
           console.log("DATA", data)
       }, [data])
-    
-      if (loading || !data?.twitterId) return <a href={`/owned/${address}`} className={'hover:bg-gray-800 p-2 -ml-2 rounded-md'}>{!!address && shortenAddress(address)}</a>
+
+      const dest = linkToCreated ? 'created' : 'owned';
+
+      if (loading || !data?.twitterId) return <a href={`/${dest}/${address}`} className={'hover:bg-gray-800 p-2 -ml-2 rounded-md'}>{!!address && shortenAddress(address)}</a>
       if (error) return <p>Error!</p>
       if (!data) return <p>Error!</p>
 
       return (
         <div className={'font-bold text-md text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-red-300'}>
-          <a href={`/owned/${address}`} className={'hover:bg-gray-800 hover:from-pink-300 hover:to-pink-400 p-2 -ml-2 rounded-md font-bold text-md text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-red-300'} >@{data.twitterId}</a>
+          <a href={`/${dest}/${address}`} className={'hover:bg-gray-800 hover:from-pink-300 hover:to-pink-400 p-2 -ml-2 rounded-md font-bold text-md text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-red-300'} >@{data.twitterId}</a>
         </div>
       )
 }
