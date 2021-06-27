1e1c7d16cf66914905e30597ca7e39fee1dbb7a9
Zak Angelle <zaknotzak@gmail.com>
2021-06-27 17:24:17 -0500

    show slim progress bar at top of page on route transition

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
 import 'tailwindcss/tailwind.css'
+import Router from 'next/router'
 import dynamic from 'next/dynamic'
-import '../styles/global.css'
+import NProgress from 'nprogress';
 import { FirebaseTrackingProvider } from "../config/firebase";
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import 'nprogress/nprogress.css';
+import '../styles/global.css';
 
 const Provider = dynamic(() => import("../state/StoreProvider"), {
   ssr: false,
@@ -13,6 +16,10 @@ const client = new ApolloClient({
   uri: "https://api.thegraph.com/subgraphs/name/jsmellz/screensaverv1"
 });
 
+NProgress.configure({ showSpinner: false });
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return (
