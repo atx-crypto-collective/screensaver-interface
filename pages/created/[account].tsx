277f32b0d20a3ecbe0eb6d34abbcfe20230565b3
Greg Leuch <gleuch@gmail.com>
2021-07-29 19:27:24 -0400

    add page titles to created and owned account pages

--- pages/created/[account].tsx
+++ pages/created/[account].tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Layout, Navbar } from '../../components'
 import { useRouter } from 'next/router'
+import Head from 'next/head'
 import axios from 'axios'
 import { Web3Provider } from '@ethersproject/providers'
 import { useWeb3React } from '@web3-react/core'
@@ -11,10 +12,14 @@ import NFT from '../../types'
 import ExploreView from '../ExploreView'
 
 const CollectionPage: React.VFC = () => {
-
+  const router = useRouter()
+  const { account } = router.query
   
   return (
     <Layout>
+      <Head>
+        <title>Screensaver.world | Created by {account.toString()}</title>
+      </Head>
       <div className={'md:mt-12 pb-8 w-11/12 mx-auto'}>
         <ExploreView created={true}/>
       </div>
