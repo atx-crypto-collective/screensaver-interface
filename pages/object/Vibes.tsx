3df0dad3859eff7651ef897459e6619514a00062
Lisa Ma <lisa@yonomi.co>
2021-08-10 19:57:39 -0500

    use wellspring, render vibes ticker, code messy af, console logs all over the damn place

--- pages/object/Vibes.tsx
+++ pages/object/Vibes.tsx
@@ -11,9 +11,27 @@ interface IProps {
   tokenId: string
 }
 
+// VIBES ticker refresh in ms
+const REFRESH_VIBES_INTERVAL = 1000;
+
+const calculateLiveInfusedVibes = (startingVibes, startDateTime, tokenInfo) => {
+  const now = new Date();
+  const msElapsed = now.getTime() - startDateTime.getTime();
+  const { dailyRate } = tokenInfo;
+  const dailyRateReadable = parseInt(ethers.BigNumber.from(dailyRate).toString()) / 1000000000000000000;
+  const vibesToAdd = dailyRateReadable * (msElapsed / (24 * 60 * 60 * 1000));
+  const claimableReadable = parseInt(ethers.BigNumber.from(startingVibes).toString()) / 1000000000000000000;
+  const updatedVibes = (claimableReadable + vibesToAdd).toFixed(3);
+  console.log(updatedVibes)
+  return updatedVibes;
+};
+
 const Vibes = ({ tokenId }) => {
-  const { account, library } = useWeb3React<Web3Provider>()
+  const { account } = useWeb3React<Web3Provider>()
+  const [tokenInfo, setTokenInfo] = useState<string>('');
+  const [startingVibes, setStartingVibes] = useState<string>('');
   const [claimableVibes, setClaimableVibes] = useState<string>('');
+  const [startDateTime, setStartDateTime] = useState<Date>();
 
   async function getVibes() {
     const contract = new ethers.Contract(
@@ -21,16 +39,25 @@ const Vibes = ({ tokenId }) => {
       VIBES_WELLSPRING_ABI,
       getNetworkLibrary(),
     )
+    console.log(tokenId);
     const tokenInfo = await contract.getToken(process.env.NEXT_PUBLIC_CONTRACT_ID, tokenId);
+    console.log({ tokenInfo })
     if (tokenInfo) {
-      const { claimable } = tokenInfo;
-      const claimableReadable = (parseInt(ethers.BigNumber.from(claimable).toString()) / 1000000000000000000).toFixed(2);
-      setClaimableVibes(claimableReadable);
+      setTokenInfo(tokenInfo);
+      setStartingVibes(tokenInfo.claimable);
+      setStartDateTime(new Date());
     } 
   }
 
+  async function getClaimableVibes() {
+    const claimableVibes = tokenInfo ? calculateLiveInfusedVibes(startingVibes, startDateTime, tokenInfo) : '';
+    setClaimableVibes(claimableVibes);
+  }
+
   useEffect(() => {
-    getVibes()
+    getVibes();
+    const h = setInterval(getClaimableVibes, REFRESH_VIBES_INTERVAL);
+    return () => clearInterval(h);
   }, [account])
 
   return (
