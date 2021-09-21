9ef71a7457dad9c89e1c559e9568051431229f3b
lilneuromancer <neuromancer.kitten@gmail.com>
2021-09-21 17:45:47 -0500

    added username and /user/<user name> link

--- pages/user/[user].tsx
+++ pages/user/[user].tsx
@@ -13,13 +13,13 @@ const UserPage: React.VFC<IProps> = ({ account }) => {
 
   // If the page is not yet generated, this will be displayed
   // initially until getStaticProps() finishes running
-  // if (router.isFallback) {
-  //   return (
-  //     <Layout>
-  //       <div>Loading...</div>
-  //     </Layout>
-  //   )
-  // }
+  if (router.isFallback) {
+    return (
+      <Layout>
+        <div>Loading...</div>
+      </Layout>
+    )
+  }
 
   return (
     <Layout>
