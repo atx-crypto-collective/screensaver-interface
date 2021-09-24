a57070c9403e0d15a209fe62c824aa25259e7486
lilneuromancer <neuromancer.kitten@gmail.com>
2021-09-23 20:16:49 -0500

    switch registers as hidden if gallery data says hidden

--- pages/manageGallery/index.tsx
+++ pages/manageGallery/index.tsx
@@ -33,6 +33,11 @@ export default function Home() {
       description: storedGallery.description,
       ids: storedGallery.ids,
     })
+
+    if (storedGallery.hidden) {
+      setHide(true)
+    }
+
   }, [storedGallery])
 
   useEffect(() => {
